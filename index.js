4ff2208bcf52024adc12cbdb3aaa5a0d07c58e60
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-06-29 11:02:53 -0400

    Inject db connection for db functions
    Jest runs tests in parallel which means that a single test database will
    eventually lead to test data being clobbered. Injecting the db means we
    are able to create databases per test file and pass them in.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const graphqlHTTP = require('express-graphql')
-const { schema } = require('./src/schema')
-const { db } = require('./src/database')
-const { server } = require('./src/server')
+import graphqlHTTP from 'express-graphql'
+import { schema } from './src/schema'
+import { Database } from 'arangojs'
+import Connection  from './src/database'
+import { server } from './src/server'
+
+const username = process.env.ARANGODB_USERNAME
+const password = process.env.ARANGODB_PASSWORD
+
+const db = Connection(new Database({
+  databaseName: 'nrcan',
+  url: `http://${username}:${password}@arangodb:8529`,
+}))
 
 server.use(
   '/graphql',
--- src/database.js
+++ src/database.js
@@ -1,21 +1,17 @@
-const { aql, Database } = require('arangojs')
+import { aql, Database } from 'arangojs'
 
-const username = process.env.ARANGODB_USERNAME
-const password = process.env.ARANGODB_PASSWORD
-
-const db = new Database({
-  databaseName: 'nrcan',
-  url: `http://${username}:${password}@arangodb:8529`,
-})
-
-module.exports.db = {
-  getHouseByID: async id => {
-    let query = aql`
+const Connection = connection => {
+  return {
+    getHouseByID: async id => {
+      let query = aql`
       FOR building IN buildings
         FILTER building._key == ${id}
           RETURN building
       `
-    let results = await db.query(query)
-    return results.next()
-  },
+      let results = await connection.query(query)
+      return results.next()
+    },
+  }
 }
+
+export default Connection
